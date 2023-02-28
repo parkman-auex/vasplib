ad3a4c74c7e17406b91c020419b6efb04e5b5be8
biaobiao711 <1225007477@qq.com>
2023-02-28 17:25:25 +0800

    Update vasplib.m
    add Pauli 2 decomp

--- bin/@vasplib/vasplib.m
+++ bin/@vasplib/vasplib.m
@@ -4275,7 +4275,8 @@ classdef vasplib < matlab.mixin.CustomDisplay
             end
             if isequal(   size(H_sym) , [4,4])
                 [~,H_sym_pauli_L] = vasplib.GammaDecomposition(H_sym);
-                Pauli_L = gamma_matric.pauli_L();
+                %Pauli_L = gamma_matric.pauli_L();
+                
                 H_sym_pauli = sym(0);
                 H_latex_pauli = 'H = ';
                 count = 0;
@@ -4295,11 +4296,56 @@ classdef vasplib < matlab.mixin.CustomDisplay
                         ChooseL(i) = false;
                     end
                 end
+                H_sym_pauli_L = [H_sym_pauli_L;Pauli_L];
+                H_sym_pauli_L = H_sym_pauli_L(:,ChooseL);
             end
-            H_sym_pauli_L = [H_sym_pauli_L;Pauli_L];
-            H_sym_pauli_L = H_sym_pauli_L(:,ChooseL);
             if isequal(   size(H_sym) , [2,2])
-
+                tmp_mat_r = real(H_sym);
+                tmp_mat_i = imag(H_sym);
+                H_sym_L = sym(zeros(1,4));
+                H_sym_pauli_L = sym(zeros(1,4));
+%                 sigma_L = pauli_matric();
+                %
+                Smat = sym([1/2,1/2,0,0; ...
+                                0,0,1,0; ...
+                                0,0,0,-1; ...
+                                1/2,-1/2,0,0]);
+%                 Smat_inv = inv(Smat);
+                %
+                syms sigma_0 sigma_x sigma_y sigma_z real;
+                Pauli_L = [sigma_0 sigma_x sigma_y sigma_z];
+                H_sym_L(1:2) = diag(H_sym);
+                H_sym_L(3) = diag(tmp_mat_r,1);
+                H_sym_L(4) = diag(tmp_mat_i,1);
+                for i = 1:numel(H_sym_L)
+                    Label_tmp = find(Smat(i,:));
+                    H_sym_pauli_L(i) = H_sym_L(Label_tmp)*Smat(i,Label_tmp)';
+                end
+%                 H_sym_pauli_L_temp = Smat*H_sym_L';
+%                 H_sym_pauli_L = H_sym_pauli_L_temp';
+                H_sym_pauli_L = simplify(H_sym_pauli_L);
+                H_sym_pauli = sym(0);
+                H_latex_pauli = 'H = ';
+                count = 0;
+                ChooseL = logical(1:4);
+                %Pauli_L = sym(Pauli_L);
+                for i = 1:4
+                    if H_sym_pauli_L(i)~=sym(0)
+                        count = count+1;
+                        H_sym_pauli = H_sym_pauli + H_sym_pauli_L(i)*sym(Pauli_L(i));
+                        str1 = latex(H_sym_pauli_L(i));
+                        str2 = latex(sym(Pauli_L(i)));
+                        if count >1
+                            H_latex_pauli = [H_latex_pauli,'+','\left(',str1,'\right)',str2];
+                        else
+                            H_latex_pauli = ['\left(',str1,'\right)',str2];
+                        end
+                    else
+                        ChooseL(i) = false;
+                    end
+                end
+                H_sym_pauli_L = [H_sym_pauli_L;Pauli_L];
+                H_sym_pauli_L = H_sym_pauli_L(:,ChooseL);
             end
         end
     end
