afde04d258dc08f40fe6f08998caf2edb39d4220
NoOne <parkman@buaa.edu.cn>
2023-08-29 08:36:01 +0800

    UpdateHR
    UpdateHR

--- bin/@HR/HR.m
+++ bin/@HR/HR.m
@@ -2462,8 +2462,12 @@ classdef HR <vasplib & matlab.mixin.CustomDisplay
                     end
                 elseif strcmp(H_hr.Type,'list')
                     wan_list =  all(ismember(H_hr.vectorL(:,H_hr.Dim+1:H_hr.Dim+2),(wan_list)),2);
-                    H_hr.HnumL=H_hr.HnumL(wan_list,:);
-                    H_hr.HcoeL=H_hr.HcoeL(wan_list,:);
+                    if H_hr.num
+                        H_hr.HnumL=H_hr.HnumL(wan_list,:);
+                    end
+                    if H_hr.coe
+                        H_hr.HcoeL=H_hr.HcoeL(wan_list,:);
+                    end
                     H_hr.vectorL=H_hr.vectorL(wan_list,:);
                     if H_hr.overlap
                         H_hr.SnumL=H_hr.SnumL(wan_list,:);
@@ -7609,6 +7613,50 @@ classdef HR <vasplib & matlab.mixin.CustomDisplay
                 elseif H_hr.num
                     disp([double(H_hr.vectorL(seq,:)),real(H_hr.HnumL(seq,:)),imag(H_hr.HnumL(seq,:))]);
                 end
+            end
+        end
+        function H_hr = rmhop(H_hr,vectorL,options)
+            arguments
+                H_hr HR
+                vectorL = [1 1];
+                options.enforce_list  = false;
+            end
+            if ~strcmp(H_hr.type, 'list')
+                H_hr = H_hr.rewrite();
+            end
+            if options.enforce_list
+
+                giveback  = false;
+            else
+                giveback  = true;
+            end
+
+            vectorList = double(H_hr.vectorL);
+            % if more?
+            if isa(vectorL,'double')
+                switch size(vectorL,2)
+                    case 2
+                        [seq] = ~(ismember(H_hr.vectorL(:,H_hr.Dim+1:H_hr.Dim+2),(vectorL),"rows"));
+                    case 3
+                        [seq] = ~(ismember(H_hr.vectorL(:,1:3),(vectorL),"rows"));
+                    case 5
+                        [seq] = ~(ismember(H_hr.vectorL(:,1:5),(vectorL),"rows"));
+                end
+            elseif isa(vectorL,'sym')
+                [seq] = find(park.strcontain(string(H_hr.HcoeL),string(vectorL)));
+            end
+            if H_hr.num
+                H_hr.HnumL=H_hr.HnumL(seq,:);
+            end
+            if H_hr.coe
+                H_hr.HcoeL=H_hr.HcoeL(seq,:);
+            end
+            H_hr.vectorL=H_hr.vectorL(seq,:);
+
+            if giveback
+                H_hr = H_hr.rewind();
+            else
+                H_hr = H_hr;
             end
         end
         function [fig,ax] = show(H_hr,mode,options)
