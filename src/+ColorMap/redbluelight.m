30025a7605737dae6812da8e7f17a5bb95799f16
fyangtcmp <fanyang_tcmp@buaa.edu.cn>
2024-06-23 09:25:07 +0800

    small update on Figs

---
+++ src/+ColorMap/redbluelight.m
@@ -0,0 +1,6 @@
+function c = redbluelight(m)
+
+c= ColorMap.customcolormap(linspace(0,1,7),...
+    {'#aa2409','#f64623','#fea290','#ffffff','#92c7fc','#258cf4','#0d59a5'},m);
+
+end
--- src/functions_plot/Figs.m
+++ src/functions_plot/Figs.m
@@ -5,8 +5,9 @@ function [f_struct,Ax] = Figs(rows, cols,opts)
 arguments
     rows double = 1
     cols double = 1
-    opts.FontName = 'Helvetica';
-    opts.FontSize = 24;
+    opts.FontName = 'Helvetica'
+    opts.FontSize = 0
+    opts.units {mustBeMember(opts.units,{'normalized','centimeters'})} = 'normalized'
 end
 %%
 figure();
@@ -17,6 +18,28 @@ f_struct.fig = gcf;
 for i = 1 : rows*cols
     nexttile(tcl,i);
 end
+%%
+switch opts.units
+    case 'normalized'
+        unit_base = [0.4,0.4];
+        unit_factor = sqrt([cols, rows]);
+        unit_base = (unit_base .* unit_factor);
+        unit_base =  unit_base/max(unit_base)*0.6;
+        set(f_struct.fig,'Units','normalized');            
+        set(f_struct.fig,'Position',[0.1 0.1 unit_base(1) unit_base(2)]);
+        if opts.FontSize==0
+            opts.FontSize = 24;
+        end
+    case 'centimeters'
+        unit_base = 7.5;
+        unit_factor = [cols, rows];
+        unit_base = (unit_base * unit_factor);
+        set(f_struct.fig,'Units','centimeters');
+        set(f_struct.fig,'Position',[15 10 unit_base(1) unit_base(2)]);
+        if opts.FontSize==0
+            opts.FontSize = 10;
+        end
+end
 
 % Populate the layout with the axes
 f_struct.axes = gobjects(rows, cols);
@@ -26,21 +49,18 @@ for m = 1:rows
         t = (m-1) * cols + n;
         f_struct.axes(m,n) = nexttile(tcl,t);
         axis(f_struct.axes(m,n),'square');
+        switch opts.units
+            case 'normalized'
+                
+            case 'centimeters'
+                set(gca,'Units','centimeters');
+        end
     end
 end
 %%
-unit_base = [0.4,0.4];
-unit_factor = sqrt([cols, rows]);
-unit_base = (unit_base .* unit_factor);
-unit_base =  unit_base/max(unit_base)*0.6;
-set(f_struct.fig,'Units','normalized');            
-set(f_struct.fig,'Position',[0.1 0.1 unit_base(1) unit_base(2)]);         
-%%
-FontName = opts.FontName;
-FontSize = opts.FontSize;
 LineWidth = 1;
 % Color = [0.9400 0.9400 0.9400];
-set( f_struct.axes,'FontSize',FontSize,'FontName',FontName,'LineWidth',LineWidth);
+set( f_struct.axes,'FontSize',opts.FontSize,'FontName',opts.FontName,'LineWidth',LineWidth);
 box( f_struct.axes,'on');
 set( f_struct.fig,'Color','w');
 hold(f_struct.axes,'on');
